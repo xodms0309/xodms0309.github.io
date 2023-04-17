c9bf67f3bcd3c422640b731bad21490426bc369a
TaeeunKim <xodms0309@naver.com>
2023-04-17 15:09:13 +0900

    chore: init blog project

---
+++ content/blog/회고/test.md
@@ -0,0 +1,8 @@
+---
+title: '테스트 글'
+date: 2020-07-02 16:21:13
+category: '회고'
+draft: false
+---
+
+테스트
---
+++ gatsby-meta-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: `타미의 개발일지`,
+  description: `프론트엔드 개발을 하며 공부한 내용을 작성합니다.`,
+  author: `타미`,
+  introduction: ``,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `xodms0309`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+}
