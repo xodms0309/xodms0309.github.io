7bae69db658661f15d16d1185f111898bc50f308
TaeeunKim <xodms0309@naver.com>
2023-04-17 15:19:28 +0900

    feat: 메타데이터 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `프론트엔드 개발을 하며 공부한 내용을 작성합니다.`,
   author: `타미`,
   introduction: ``,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://xodms0309.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `xodms0309`, // Your GitHub account
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'xodms0309/xodms0309.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
